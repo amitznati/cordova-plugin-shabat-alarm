70a7c4f7cf3edb80f3d57e125d8aa0794381a8ed
amitznati-work-hp <amitznati@gmail.com>
2020-12-17 15:29:19 +0200

    Update ShabatAlarms.js

--- www/ShabatAlarms.js
+++ www/ShabatAlarms.js
@@ -1,5 +1,6 @@
-var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'ShabatAlarms', 'coolMethod', [arg0]);
-};
+module.exports = {
+    coolMethod: function (arg0, success, error) {
+        cordova.exec(success, error, 'ShabatAlarms', 'coolMethod', [arg0]);
+    }
+}
