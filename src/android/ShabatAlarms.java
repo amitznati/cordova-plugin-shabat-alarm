ed131d55de020392ec11ab866e03b0feafec10b7
amitznati-work-hp <amitznati@gmail.com>
2020-12-17 14:39:48 +0200

    refactor

--- src/android/ShabatAlarms.java
+++ src/android/ShabatAlarms.java
@@ -1,4 +1,4 @@
-package com.az.cordova.plugin.shabatalarm;
+package com.az.cordova.plugin.shabatalarms;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
@@ -10,7 +10,7 @@ import org.json.JSONObject;
 /**
  * This class echoes a string called from JavaScript.
  */
-public class cordova-plugin-shabat-alarm extends CordovaPlugin {
+public class ShabatAlarms extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
